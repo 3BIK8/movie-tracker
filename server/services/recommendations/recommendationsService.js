e49ce62c3e6a50d1adacb5b0748a322efc32a514
Abdeslam <lahjoujiabdessalam@gmail.com>
2026-09-10 04:04:27 +0100

    feat(recommendations): integrate deterministic diversity reranking

--- server/services/recommendations/recommendationsService.js
+++ server/services/recommendations/recommendationsService.js
@@ -3,11 +3,13 @@ import { getMediaConnections } from "./connectionExtractor.js";
 import { analyzeHistory } from "./historyAnalyzer.js";
 import { generateCandidates } from "./candidateService.js";
 import { scoreCandidates } from "./recommendationScorer.js";
+import { diversifyRankedCandidates } from "./recommendationDiversifier.js";
 import { normalizeWatchHistory } from "../../utils/mediaIdentity.js";
 import { mapWithConcurrency } from "../../utils/runWithConcurrency.js";
 
 const RECOMMENDATION_LIMIT = 100;
 const HISTORY_ENRICHMENT_CONCURRENCY = 6;
+const DIVERSITY_LAMBDA = 0.8;
 
 export async function analyzeWatchHistory(history) {
   const canonicalHistory = normalizeWatchHistory(history);
@@ -63,11 +65,23 @@ export async function analyzeWatchHistory(history) {
     scoreCandidates(tvCandidates, enrichedHistory),
   ];
 
+  const diversifiedMovies = diversifyRankedCandidates(
+    scoredMovies,
+    RECOMMENDATION_LIMIT,
+    DIVERSITY_LAMBDA,
+  );
+
+  const diversifiedTv = diversifyRankedCandidates(
+    scoredTv,
+    RECOMMENDATION_LIMIT,
+    DIVERSITY_LAMBDA,
+  );
+
   return {
     profile,
     recommendations: {
-      movies: scoredMovies,
-      tv: scoredTv,
+      movies: diversifiedMovies,
+      tv: diversifiedTv,
     },
   };
 }
