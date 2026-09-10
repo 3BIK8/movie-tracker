47f890d182dee4cc12d2805b524198b88ab8948d
Abdeslam <lahjoujiabdessalam@gmail.com>
2026-09-10 02:50:50 +0100

    fix: remove unused network effect import

--- client/src/pages/Network.jsx
+++ client/src/pages/Network.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useWatchHistory } from "../hooks/useWatchHistory";
 import { useConnectionSearch } from "./network/useConnectionSearch";
 import { useNetworkGraph } from "./network/useNetworkGraph";
