9cb23bf4db00be8e491321af764b058819cca936
Abdeslam <lahjoujiabdessalam@gmail.com>
2026-09-13 03:31:55 +0100

    chore: allow intentional network effect state synchronization

--- client/src/pages/network/useNetworkGraph.js
+++ client/src/pages/network/useNetworkGraph.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/set-state-in-effect */
 import { useEffect, useRef, useState } from "react";
 import cytoscape from "cytoscape";
 import { getWatchHistoryNetwork } from "../../services/api";
